608308ae2ac17b79147e44c68a487ac91a804bbf
Devendra Tewari <devendra.tewari@gmail.com>
2018-03-28 13:51:21 -0300

    keep container

--- run-docker.ps1
+++ run-docker.ps1
@@ -1,1 +1,1 @@
-docker run -i -t --rm -v ${PWD}:/srv/jekyll -p "4000:4000" jekyll/jekyll jekyll serve --watch --incremental
+docker run -i -t -v ${PWD}:/srv/jekyll -p "4000:4000" jekyll/jekyll jekyll serve --watch --incremental
