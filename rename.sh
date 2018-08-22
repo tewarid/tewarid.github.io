9a9899cb53536ab3af486c352c3914f6ce2e6ce8
Devendra Tewari <devendra.tewari@gmail.com>
2018-08-22 06:39:33 -0300

    improve maintainability

--- rename.sh
+++ rename.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-for i in *.md; do v=`grep title: $i`; v=${v:7}; v=`echo ${v} | awk '{print tolower($0)}'`; v=`echo ${v//[ \/?\":]/-}`; mv ${i} ${i:0:10}-$v.md; done
+for i in *.md
+do
+    v=`grep title: $i`
+    v=${v:7}
+    v=`echo ${v} | awk '{print tolower($0)}'`
+    v=`echo ${v//[ \/?\":]/-}`
+    mv ${i} ${i:0:10}-$v.md
+done
