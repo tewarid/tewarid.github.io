441297cbda5d4e5d74d856c8c971b6cda082e6bf
Devendra Tewari <devendra.tewari@gmail.com>
2018-10-29 14:41:07 -0300

    remove container when done

--- run-docker.sh
+++ run-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -i -t -v "$PWD":/srv/jekyll -p "4000:4000" jekyll/jekyll jekyll serve --watch --incremental
+docker run -i -t --rm -v "$PWD":/srv/jekyll -p "4000:4000" jekyll/jekyll jekyll serve --watch --incremental
