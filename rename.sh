1292f51f120a2504a0e598f295e5be452923fcb9
Devendra Tewari <devendra.tewari@gmail.com>
2022-03-10 09:00:09 -0300

    handle whitespace in file names

--- rename.sh
+++ rename.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in *.md
 do
-    v=`grep title: $i`
+    v=`grep title: "$i"`
     v=${v:7}
     # convert to lower case
     v=`echo ${v} | awk '{print tolower($0)}'`
@@ -9,5 +9,5 @@ do
     v=`echo ${v//[\"]/}`
     # replace invalid file name characters with dash
     v=`echo ${v//[ \/?\":]/-}`
-    mv ${i} ${i:0:10}-$v.md
+    mv "${i}" "${i:0:10}-$v.md"
 done
